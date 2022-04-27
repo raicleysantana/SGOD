9a2df1319452e7bb61deccb951180d82fe7ef486
raicleysantana <raicleysantana1@gmail.com>
2022-04-27 01:50:38 -0400

    initial commit

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
-<html>
-    <body>
-        <h1>Hello word</h1>
-    </body>
+<!DOCTYPE html>
+<html dir="ltr" lang="pt-BR ">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+    <meta name="robots" content="noindex,nofollow"/>
+    <title>SGODP - SISTEMA DE GESTÃO DE OCORRÊNCIAS DISCIPLINARES PEDAGOGICAS</title>
+
+    <!-- Suporte para HTML5 Shim e Respond.js IE8 de elementos HTML5 e consultas de mídia -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <?php include "layouts/header.php"; ?>
+</head>
+
+<body>
+<?php include "pages/home/index.php"; ?>
+
+<?php include "layouts/footer.php"; ?>
+
+<script>
+    $(function () {
+
+    });
+</script>
+</body>
+
 </html>
---
+++ layouts/header.php
@@ -0,0 +1,13 @@
+<?php
+include "config/conf.php";
+?>
+
+<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.min.css">
+<!--<link
+        rel="stylesheet"
+        href="<?/*= BASE_URL */?>/assets/css/material-design-iconic-font/css/materialdesignicons.min.css"
+>
+<link
+        rel="stylesheet"
+        href="<?/*= BASE_URL */?>/assets/css/font-awesome/css/fontawesome-all.min.css"
+>-->
